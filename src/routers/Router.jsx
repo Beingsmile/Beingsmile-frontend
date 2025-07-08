487323b848dc07acfde1d083eab8464ba95f9a6a
saima akter <Saimaakterrichi06@google.com>
2025-07-08 20:38:57 +0600

    setup done for react router

---
+++ src/routers/Router.jsx
@@ -0,0 +1,13 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import Home from "../pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />, 
+  },
+]);
+
+export default router;
