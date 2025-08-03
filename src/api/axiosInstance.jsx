fafdcb0e73a960a4ea9170141f586d98027a0041
Atik471 <atiksafinmd1@gmail.com>
2025-08-03 20:59:19 +0600

    changed api url

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create(
     {
-        baseURL: "https://beingsmile-api.onrender.com/api/",
+        baseURL: "https://api.beingsmile.org/api/", 
         withCredentials: true,
         headers: {
             "Content-Type": "application/json",
